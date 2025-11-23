15cd4444c701aaf0a335c66cd18ad7486e546015
Amitkarol <Amit@DESKTOP-H2FEIKQ>
2025-11-23 17:39:55 +0200

    Add client directory into the server and did some changes in server.js

--- server.js
+++ server.js
@@ -1,27 +1,43 @@
-import express from "express"
-import mongoose from "mongoose"
-import bodyParser from "body-parser"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import productRoutes from "./Routes/productRoutes.js";
-
-
-const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use("/api", productRoutes);
 
 dotenv.config();
 
-const PORT = process.env.PORT || 7000;
-const MONGO_URL = process.env.MONGO_URL;
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-mongoose.connect(MONGO_URL)
-        .then(() => {
-            console.log("DB connected successfully");
-            app.listen(PORT,() => {
-                    console.log(`Server is running on port :${PORT}`);
-            })
-        })
-        .catch((error) => console.log(error));
+// --- MIDDLEWARE ---
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// --- API ROUTES ---
+app.use("/api", productRoutes);
+
+// --- SERVE REACT BUILD ---
+app.use(express.static(path.join(__dirname,"client")));
+
+// --- CATCH ALL FOR REACT ROUTER ---
+app.get(/^(?!\/api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "index.html"));
+});
+
+// --- CONNECT TO DB AND START SERVER ---
+const PORT = process.env.PORT || 8000;
+const MONGO = process.env.MONGO_URL;
+
+mongoose
+  .connect(MONGO)
+  .then(() => {
+    console.log("DB connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  })
+  .catch((err) => console.log("MongoDB Error: ", err));
